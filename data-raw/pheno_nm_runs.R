7c4bd10ead0b7d2af7d56f8d3959e0652a7c6e5d
Prybylski <john.prybylski@pfizer.com>
2024-09-10 16:38:49 -0400

    start processing pheno examples

--- data-raw/pheno_nm_runs.R
+++ data-raw/pheno_nm_runs.R
@@ -2,7 +2,40 @@
 ## pheno_sd dataset (as it is called in nlmixr2)
 
 # NONMEM was run external to this project, but used the data file
-# from nlmixr2. The command to create it was simply:
+# from nlmixr2data. The command to create it was simply:
 # readr::write_csv(nlmixr2data::pheno_sd, "inst/pheno_nm/data.csv")
+
+# The run results are compressed into a tree, which will be read manually
+# If there is ever a function to ingest set from a directory, this approach
+# should be replaced by that approach.
+# TODO: update with directory consumer
+tmpdir <- tempdir()
+tree_tar <- here::here("inst", "pheno_nm", "clean_tree.tar.gz")
+
+untar(tree_tar, exdir = tmpdir)
+
+model_output_dirs <- list.dirs(tmpdir, recursive = TRUE) %>%
+  .[grepl("Step \\d+", .)]
+
+# process directory function
+dir_proc <- function (dir) {
+  runno <- stringr::str_extract(dir," (\\d+)$", 1)
+  childno <- model_output_dirs %>%
+    .[!.==dir] %>%
+    .[stringr::str_detect(., stringr::fixed(dir))] %>%
+    stringr::str_replace(stringr::fixed(paste0(dir,"/")), "") %>%
+    .[!stringr::str_detect(., stringr::fixed("/"))] %>%
+    stringr::str_extract(" (\\d+)$", 1)
+
+  list(
+    children = childno,
+    xpdb = xpose::xpose_data(runno, dir=dir) %>% as_xpdb_x()
+  )
+}
+
+# Flat list of objects
+flat_xpdbs <- purrr::map(model_output_dirs, dir_proc)
+
+#....
 
 usethis::use_data(pheno_nm_runs, overwrite = TRUE, compress = "gzip")
